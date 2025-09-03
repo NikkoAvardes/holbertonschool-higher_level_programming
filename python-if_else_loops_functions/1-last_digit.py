c7ee7dbeea409216963abc86573a4996e92fa966
NikkoAvardes <11460@holbertonstudents.com>
2025-09-03 09:19:21 +0200

    Correction de l'indentation dans 0-positive_or_negative.py, 1-last_digit.py, 2-print_alphabet.py et 3-print_alphabt.py pour respecter les conventions de style.

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,18 +2,16 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-	 lastDigit = (number * -1 % 10) * -1
+    lastDigit = (number * -1 % 10) * -1
 else:
-	lastDigit = number % 10
-
+    lastDigit = number % 10
 if (lastDigit) > 5:
-	print(f"Last digit of {number} is {lastDigit} "
-	   	  "and is greater than 5")
-
+    print(f"Last digit of {number} is {lastDigit} "
+          "and is greater than 5")
 elif (lastDigit) == 0:
-	print(f"Last digit of {number} is {lastDigit} and is 0")
+    print(f"Last digit of {number} is {lastDigit} and is 0")
 
 if (lastDigit) < 6:
-	if (lastDigit) != 0:
-		print(f"Last digit of {number} is {lastDigit} "
-			  "and is less than 6 and not 0")
+    if (lastDigit) != 0:
+        print(f"Last digit of {number} is {lastDigit} "
+              "and is less than 6 and not 0")
