4688859a72c6e1802d9e8a5823a3ca614e8cf7ef
NikkoAvardes <11460@holbertonstudents.com>
2025-09-05 10:08:05 +0200

    Réorganisation du code dans 2-args.py pour gérer l'affichage des arguments en fonction de leur nombre.

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,11 @@
 if __name__ == "__main__":
     import sys
     n = len(sys.argv) - 1
-
-    print("{} argument:".format(n))
-for i in range(1, n + 1):
-    print("{}: {}".format(i, sys.argv[i]), end='\n')
+    if n == 0:
+        print("0 arguments.")
+    elif n == 1:
+        print("1 argument:")
+    else:
+        print("{} argument:".format(n))
+    for i in range(1, n + 1):
+        print("{}: {}".format(i, sys.argv[i]), end='\n')
