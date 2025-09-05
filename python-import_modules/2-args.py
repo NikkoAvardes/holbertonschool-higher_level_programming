6a97060fe1e83475369510d2b5ce5edbe561c58e
NikkoAvardes <11460@holbertonstudents.com>
2025-09-05 10:28:19 +0200

    Correction du message d'affichage pour le nombre d'arguments dans 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     elif n == 1:
         print("1 argument:")
     else:
-        print("{} argument:".format(n))
+        print("{} arguments:".format(n))
 
     for i in range(1, n + 1):
         print("{}: {}".format(i, sys.argv[i]))
