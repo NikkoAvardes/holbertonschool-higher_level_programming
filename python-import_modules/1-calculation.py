7dc481c5ee65d1f436582d9e6abe5e7abc2d5574
NikkoAvardes <11460@holbertonstudents.com>
2025-09-05 09:15:50 +0200

    Réorganisation du code pour utiliser des variables descriptives pour les résultats des opérations

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,10 +2,10 @@
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-	a = 10
-	b = 5
-	c = "{} + {} = {}".format(a, b, add(a, b))
-	k = "{} - {} = {}".format(a, b, sub(a, b))
-	l = "{} * {} = {}".format(a, b, mul(a, b))
-	p = "{} / {} = {}".format(a, b, div(a, b))
-	print(c,k,l,p, sep="\n")
+    a = 10
+    b = 5
+    result_add = "{} + {} = {}".format(a, b, add(a, b))
+    result_sub = "{} - {} = {}".format(a, b, sub(a, b))
+    result_mul = "{} * {} = {}".format(a, b, mul(a, b))
+    result_div = "{} / {} = {}".format(a, b, div(a, b))
+    print(result_add, result_sub, result_mul, result_div, sep="\n")
