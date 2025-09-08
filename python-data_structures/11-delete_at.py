6329f090e9e3c953514f9fdd7e7fbced29bac830
NikkoAvardes <11460@holbertonstudents.com>
2025-09-08 21:00:37 +0200

    Correction de la fonction delete_at pour éviter la création d'une nouvelle liste, simplifiant ainsi le code et améliorant l'efficacité.

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,7 +3,6 @@ def delete_at(my_list=[], idx=0):
     if my_list is None:
         my_list = []
     if idx < 0 or idx >= len(my_list):
-        return my_list[:]
-    new_list = my_list[:]
-    del new_list[idx]
-    return new_list
+        return my_list
+    del my_list[idx]
+    return my_list
