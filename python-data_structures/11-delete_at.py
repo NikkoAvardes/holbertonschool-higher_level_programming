9b9be025121ecfa87d04b91be3fbf25906bce1c5
NikkoAvardes <11460@holbertonstudents.com>
2025-09-08 20:55:26 +0200

    Correction de la gestion des indices dans delete_at pour éviter les erreurs et améliorer la robustesse de la fonction.

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
+    if my_list is None:
+        my_list = []
+    if idx < 0 or idx >= len(my_list):
+        return my_list[:]
     new_list = my_list[:]
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
-    new_list.remove(idx)
+    del new_list[idx]
     return new_list
