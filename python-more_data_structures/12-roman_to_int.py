670f270ae1519e1aaabd45954806e85efdd9395b
NikkoAvardes <11460@holbertonstudents.com>
2025-09-10 12:56:52 +0200

    Correction de la vérification de type dans la fonction roman_to_int et utilisation de get pour récupérer les valeurs du dictionnaire.

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or roman_string is None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                   'C': 100, 'D': 500, 'M': 1000}
     total = 0
     prev = 0
     for char in reversed(roman_string):
-        value = roman_dict[char]
+        value = roman_dict.get(char, 0)
         if value < prev:
             total -= value
         else:
