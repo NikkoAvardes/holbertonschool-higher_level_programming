1546f61320afd76541cc75b5dd9da6ac333edf15
NikkoAvardes <11460@holbertonstudents.com>
2025-09-12 15:53:57 +0200

    Correction de la documentation de la fonction add_integer pour améliorer la clarté.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,8 +6,8 @@ after casting them to integers.
 """
 
 def add_integer(a, b=98):
-    """
-    Adds 2 integers.
+    """Adds 2 integers.
+    
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
