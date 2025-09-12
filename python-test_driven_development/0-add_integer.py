5213da91a5ef6abef83c857e7c426071c4c1aa01
NikkoAvardes <11460@holbertonstudents.com>
2025-09-12 18:12:01 +0200

    Ajout de la documentation et des tests pour les fonctions say_my_name, print_square et text_indentation, ainsi que des vérifications d'erreurs pour améliorer la robustesse et la clarté.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,24 +5,12 @@ This module contains a function that adds two integers or floats
 after casting them to integers.
 """
 
-
 def add_integer(a, b=98):
     """Adds 2 integers.
     
     """
-    # Check for NaN values
-    if (isinstance(a, float) and a != a) or (isinstance(b, float) and b != b):
-        raise ValueError("cannot convert float NaN to integer")
-    
-    # Check for infinity values
-    if (isinstance(a, float) and (a == float('inf') or a == float('-inf'))) or \
-       (isinstance(b, float) and (b == float('inf') or b == float('-inf'))):
-        raise OverflowError("cannot convert float infinity to integer")
-    
-    # Check type validation
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
     return int(a) + int(b)
