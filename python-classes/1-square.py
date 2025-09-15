57bf62e775dbd194c2f78ea5af2c8f7c28141ee6
NikkoAvardes <11460@holbertonstudents.com>
2025-09-15 11:18:42 +0200

    Ajout d'une documentation et modification du constructeur de la classe Square pour accepter une taille par défaut

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""Module qui définit une classe Square avec attribut privé size"""
+
+
 class Square:
-	
-	def __init__(self, size):
+	"""Classe qui définit un carré avec taille privée"""
+
+	def __init__(self, size=None):
+		"""Initialise un carré avec une taille donnée
+		
+		Args:
+			size: La taille du carré
+		"""
 		self.__size = size
