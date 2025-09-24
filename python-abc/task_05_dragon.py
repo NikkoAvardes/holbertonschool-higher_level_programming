02bc6bdca9c65f11b4aebac81dbc66c5f4764d52
NikkoAvardes <11460@holbertonstudents.com>
2025-09-24 11:21:31 +0200

    Ajout des mixins SwimMixin et FlyMixin pour implémenter les capacités de nage et de vol dans la classe Dragon.

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 """The Mystical Dragon - Mastering Mixins"""
 
-# Votre code ici
+
+class SwimMixin:
+    """Mixin for swimming capability."""
+
+    def swim(self):
+        print("The creature swims!")
+
+
+class FlyMixin:
+    """Mixin for flying capability."""
+
+    def fly(self):
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    """Dragon class using mixins."""
+
+    def roar(self):
+        print("The dragon roars!")
