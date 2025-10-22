7d641bad1f37decacacf4bc2ae976f00f4f0f4ff
NikkoAvardes <11460@holbertonstudents.com>
2025-10-22 09:44:45 +0200

    Ajout de la requête SQL pour lister toutes les villes de Californie en utilisant une sous-requête.

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,1 +1,7 @@
 -- Liste toutes les villes de Californie en utilisant une sous-requête (sans JOIN)
+SELECT cities.name
+FROM cities
+WHERE cities.id IN (
+	SELECT id FROM states WHERE name = "California"
+)
+ORDER BY cities.id ASC;
