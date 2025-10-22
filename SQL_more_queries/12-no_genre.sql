3b733470248780320359c827f1ac99fc766c3043
NikkoAvardes <11460@holbertonstudents.com>
2025-10-22 13:25:09 +0200

    Correction de la requête SQL pour lister les émissions sans genre associé en sélectionnant genre_id au lieu de show_id.

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- Liste toutes les émissions qui n'ont aucun genre lié
-SELECT tv_shows.title, tv_show_genres.show_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
