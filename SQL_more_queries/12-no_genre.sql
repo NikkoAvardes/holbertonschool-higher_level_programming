95ec19a7ec6961810296b12781c08de972d19e03
NikkoAvardes <11460@holbertonstudents.com>
2025-10-22 13:22:44 +0200

    Ajout de la requête SQL pour lister toutes les émissions sans genre associé

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,1 +1,6 @@
 -- Liste toutes les émissions qui n'ont aucun genre lié
+SELECT tv_shows.title, tv_show_genres.show_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
