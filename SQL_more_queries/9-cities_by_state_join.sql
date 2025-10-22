75d323b0a65095f5161eb3ec1010b1654b547c29
NikkoAvardes <11460@holbertonstudents.com>
2025-10-22 10:14:03 +0200

    Ajout de la requête SQL pour lister toutes les villes avec leurs états en utilisant JOIN

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,1 +1,8 @@
 -- Liste toutes les villes avec leurs états en utilisant JOIN
+SELECT
+cities.id,
+cities.name,
+states.name
+FROM cities
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
