28374b28ed734f994751c821803074c011a2e015
NikkoAvardes <11460@holbertonstudents.com>
2025-10-22 13:41:30 +0200

    Ajout de la requête SQL pour lister tous les genres et le nombre d'émissions liées à chaque genre.

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,1 +1,6 @@
 -- Liste tous les genres et le nombre d'émissions liées à chaque genre
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_show_genres
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
