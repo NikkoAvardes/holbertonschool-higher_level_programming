d0da38e9ccdda13fc0d62fe4de0a53b8c17dc297
NikkoAvardes <11460@holbertonstudents.com>
2025-10-22 11:20:48 +0200

    Ajout de la requête SQL pour lister toutes les émissions avec leurs genre_id.

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,1 +1,5 @@
 -- Liste toutes les émissions avec leurs genre_id (affiche NULL si pas de genre)
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
