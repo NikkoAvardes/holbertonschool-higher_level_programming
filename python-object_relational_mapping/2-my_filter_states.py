8213322b31dd5afbf66c52fbab284f22041ea60e
NikkoAvardes <11460@holbertonstudents.com>
2025-10-23 15:08:36 +0200

    Correction de la documentation et des paramètres de connexion dans le script de filtrage des états

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
+"""
+Filtre les états par nom fourni en argument (format string).
+
+Usage: ./2-my_filter_states.py mysql_user mysql_password database state_name
+"""
+
 import MySQLdb
 import sys
 
 
 def main():
+    """Se connecte à MySQL et affiche l'état correspondant au nom."""
     db = MySQLdb.connect(
         host='localhost',
         user=sys.argv[1],
-        paswrd=sys.argv[2],
-        db=sys.argv[3],
-        search=sys.argv[4]
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = ? ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+                .format(sys.argv[4]))
 
     rows = cur.fetchall()
     for row in rows:
