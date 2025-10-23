a9ad090511e862122bc6540f1322728cb752f4af
NikkoAvardes <11460@holbertonstudents.com>
2025-10-23 11:01:51 +0200

    Correction de la requête SQL pour trier les états par ID et suppression du port par défaut dans la connexion à la base de données

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -24,7 +24,6 @@ def main():
     """
     db = MySQLdb.connect(
         host="localhost",
-        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
@@ -32,7 +31,7 @@ def main():
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
     rows = cur.fetchall()
 
