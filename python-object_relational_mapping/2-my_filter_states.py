19a67be26d6936b8c7c7f1cde7a47d50e03eb6f8
NikkoAvardes <11460@holbertonstudents.com>
2025-10-23 15:03:22 +0200

    Ajout de la fonction principale pour se connecter à la base de données et récupérer les états par nom

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,27 @@
-# Affiche les états correspondant au nom fourni en argument (format string)
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+
+def main():
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        paswrd=sys.argv[2],
+        db=sys.argv[3],
+        search=sys.argv[4]
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = ? ORDER BY id ASC")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
