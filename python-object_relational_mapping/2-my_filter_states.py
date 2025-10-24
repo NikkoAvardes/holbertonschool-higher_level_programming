05b69230501c8d1f24ec888550e54fe2919b286a
NikkoAvardes <11460@holbertonstudents.com>
2025-10-24 12:19:40 +0200

    Refactor le script pour améliorer la lisibilité en stockant les arguments de ligne de commande dans des variables avant de les utiliser dans la connexion à la base de données.

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -21,17 +21,22 @@ def main():
     states with names matching the fourth argument. The query uses string
     formatting which creates a potential SQL injection vulnerability.
     """
+    user_name = sys.argv[1]
+    user_passwd = sys.argv[2]
+    data_base = sys.argv[3]
+    state_name = sys.argv[4]
+
     db = MySQLdb.connect(
         host='localhost',
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=user_name,
+        passwd=user_passwd,
+        db=data_base
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' "
-                "ORDER BY id ASC".format(sys.argv[4]))
+    query = ("SELECT * FROM states WHERE name = BINARY '{}' ORDER BY id ASC")
+    cur.execute(query.format(state_name,))
 
     rows = cur.fetchall()
     for row in rows:
