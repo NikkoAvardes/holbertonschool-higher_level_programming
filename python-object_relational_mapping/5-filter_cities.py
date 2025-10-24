733be98d3c38747c79bf92dc8f518f9e70355133
NikkoAvardes <11460@holbertonstudents.com>
2025-10-24 12:35:59 +0200

    Correction de l'ordre de tri dans la requête SQL pour afficher les villes par état

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -41,7 +41,7 @@ def main():
 
     query = ("SELECT cities.name FROM cities "
              "JOIN states ON cities.state_id = states.id "
-             "WHERE states.name = %s ORDER BY cities.id ASC")
+             "WHERE states.name = %s ORDER BY states.id ASC")
 
     cur.execute(query, (state_name,))
 
