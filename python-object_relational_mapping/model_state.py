c9100c26a726958b033168598a4e666e0c337606
NikkoAvardes <11460@holbertonstudents.com>
2025-10-24 14:21:39 +0200

    Ajout de la définition du modèle State avec SQLAlchemy pour la base de données MySQL

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,1 +1,14 @@
-# Définit le modèle State avec SQLAlchemy (id, name, Base)
+#!/usr/bin/python3
+"""Define a State model for MySQL database using SQLAlchemy"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Representation of a state table in MySQL"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
