9b7cf425f704ffd43058c4fff3551e227d3344a7
NikkoAvardes <11460@holbertonstudents.com>
2025-10-24 18:21:37 +0200

    Ajout d'un script pour insérer un nouvel état dans la base de données MySQL

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(sys.argv[1],
+                sys.argv[2],
+                sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    state = session.query(State).order_by(State.id.desc()).first()
+    if state:
+        print(f"{state.id}")
+
+    session.close()
