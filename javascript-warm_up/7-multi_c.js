6e8f023b2f000cc0434132fc8b4af56737378137
NikkoAvardes <11460@holbertonstudents.com>
2025-11-07 17:00:23 +0100

    Ajout du fichier 7-multi_c.js pour afficher plusieurs occurrences de "C is fun"

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const n = parseInt(process.argv[2]);
+if (isNaN(n)) {
+  console.log('Missing number of occurrences');
+} else {
+  let res = '';
+  let x = 0;
+  while (x < n) {
+    res += 'C is fun\n';
+    x++;
+  }
+  console.log(res.trimEnd());
+}
