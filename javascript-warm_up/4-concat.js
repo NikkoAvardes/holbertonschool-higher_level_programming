c196406835ea76f9af53e0ae32e0ff8d1648707c
NikkoAvardes <11460@holbertonstudents.com>
2025-11-07 15:56:03 +0100

    Modification de la syntaxe de console.log pour une meilleure lisibilité dans 4-concat.js

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,4 +2,4 @@
 const { argv } = require('node:process');
 const firstArgv = argv[2];
 const secondArgv = argv[3];
-console.log(`${firstArgv} is ${secondArgv}`);
+console.log(firstArgv, 'is', secondArgv);
