b81c722d3b8c3a85e386718505512cb37b74383a
NikkoAvardes <11460@holbertonstudents.com>
2025-11-07 14:58:40 +0100

    Amélioration de l'indentation dans 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 if (argv[3] != null) {
-	console.log('Arguments found');
+    console.log('Arguments found');
 }
 else if (argv[2] != null) {
-	console.log('Argument found');
+    console.log('Argument found');
 }
 else {
-	console.log('No argument');
+    console.log('No argument');
 }
