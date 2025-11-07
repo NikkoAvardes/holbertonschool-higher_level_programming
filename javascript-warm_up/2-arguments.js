bf65eef9ba45cb5695defa2cb50bfa8d1d3b0a09
NikkoAvardes <11460@holbertonstudents.com>
2025-11-07 14:15:33 +0100

    Réorganisation de la logique de gestion des arguments dans 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
-	console.log('No argument');
-} 
-else if (process.argv.length === 3) {
-	console.log('Argument found');
-} 
-else {
+const { argv } = require('node:process');
+if (argv.length > 3) {
 	console.log('Arguments found');
+}
+else if (argv.length === 3) {
+	console.log('Argument found');
+}
+else {
+	console.log('No argument');
 }
