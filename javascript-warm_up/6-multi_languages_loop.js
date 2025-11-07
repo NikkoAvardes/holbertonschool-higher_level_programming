975f2204a93343993e3e1cf022ce833ba5574b1c
NikkoAvardes <11460@holbertonstudents.com>
2025-11-07 16:38:45 +0100

    Ajout du fichier 6-multi_languages_loop.js pour afficher plusieurs langages de programmation

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const myVar1 = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'JavaScript is amazing';
+const myVar4 = [myVar1, myVar2, myVar3];
+for (const x of myVar4) {
+  console.log(x);
+}
