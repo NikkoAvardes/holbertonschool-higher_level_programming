2080dc827a0648387db51a9fbe466ae228440e5b
NikkoAvardes <11460@holbertonstudents.com>
2025-11-12 10:42:02 +0100

    Ajout des fichiers de base pour une application Flask avec des routes pour les pages d'accueil, à propos et de contact, ainsi que des modèles HTML pour chaque page, incluant des en-têtes et pieds de page réutilisables.

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Basic Flask application with Jinja templates."""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """Home page route."""
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    """About page route."""
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    """Contact page route."""
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
