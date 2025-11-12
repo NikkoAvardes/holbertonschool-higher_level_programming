f86bd111ea7cd92fc66f741dc7610d7f46922fd3
NikkoAvardes <11460@holbertonstudents.com>
2025-11-12 11:30:59 +0100

    Ajout des fichiers HTML pour les pages About, Contact et Items, avec des modifications pour inclure des en-têtes et pieds de page, ainsi que l'implémentation de la logique de l'application Flask pour gérer les routes.

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Flask application with dynamic content using loops and conditions."""
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """Home page route."""
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    """About page route."""
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    """Contact page route."""
+    return render_template('contact.html')
+
+
+@app.route('/items')
+def items():
+    """Items page route."""
+    with open('items.json', 'r') as f:
+        data = json.load(f)
+        items_list = data.get('items', [])
+    return render_template('items.html', items=items_list)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
